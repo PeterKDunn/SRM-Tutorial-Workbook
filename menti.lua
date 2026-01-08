c4bdf944ebdf3ccdbc58d7c988bd4f437a42aed5
Peter Dunn <pdunn2@usc.edu.au>
2026-01-08 16:43:06 +1000

    Next try to make icons clickable

---
+++ menti.lua
@@ -0,0 +1,15 @@
+function Div(el)
+  if el.classes:includes('mentiQuestion') then
+    -- Get the image path and the target URL
+    local icon_path = "icons/iconmonstr-computer-3-240-BLUE.png"
+    local url = "https://your-target-url.com"
+    
+    if FORMAT:match 'html' then
+      -- Inject a link element at the end of the div for HTML
+      local link_icon = pandoc.RawBlock('html', 
+        '<a href="' .. url .. '" target="_blank" class="menti-link"></a>')
+      el.content:insert(link_icon)
+    end
+  end
+  return el
+end
