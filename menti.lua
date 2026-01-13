6c81bcba285fd2ee46f96cbd40d6a62ff146c681
Peter Dunn <pdunn2@usc.edu.au>
2026-01-13 20:46:01 +1000

    Update to new menti link

--- menti.lua
+++ menti.lua
@@ -3,7 +3,7 @@ function Div(el)
     if FORMAT:match 'html' then
       -- Path relative to your final HTML files
       local img_path = "../icons/iconmonstr-computer-3-240-BLUE.png"
-      local url = "https://www.mentimeter.com/app/folder/2244819"
+      local url = "https://www.mentimeter.com/app/folder/2889277"
       
       local link_html = '<a href="' .. url .. '" target="_blank" class="menti-link">' ..
                         '<img src="' .. img_path .. '" style="width:100%; height:100%; object-fit:contain;">' ..
